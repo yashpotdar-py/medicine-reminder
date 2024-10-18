325b6446e8d4b85efe75a98dab14b7594b263907
UtkarshPingale <utkarshpingale11@gamil.com>
2024-10-18 18:52:23 +0530

    make changes in reminder and a delay in reminder

--- backend/remiders.py
+++ backend/remiders.py
@@ -1,7 +1,14 @@
 import pyttsx3
+import time
 
 def text_to_speech_reminder(medicine_name, dosage):
     engine = pyttsx3.init()
-    reminder_message = f"It is time to take {dosage} of {medicine_name}."
-    engine.say(reminder_message)
-    engine.runAndWait()
+    reminder_message = f"It is time to take {dosage} of {medicine_name}"
+    
+    for _ in range(3):  # Repeat the message 3 times
+        engine.say(reminder_message)
+        engine.runAndWait()
+        time.sleep(1)  # Delay of 1 seconds between reminders
+
+if __name__ == "__main__":
+    text_to_speech_reminder("utkarsh", 2)
